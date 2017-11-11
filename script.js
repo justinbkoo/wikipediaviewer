3b3069c4e36930b3f00c1134dc7121408f22e266
Justin Koo <bjustinkoo@gmail.com>
2017-11-10 23:12:40 -0600

    Made "searchButtonOnClick" handle everything: 1.) Makes ajax call to request articles relating to the entered query 2.) On success of previous ajax call, makes another ajax call to request "extracts" (summaries) of corresponding articles 3.) Updates "results" html to display retrived article titles and summaries
    Previously had first ajax call's "success" paramter be a named function, now it is anonymous
    
    Currently displays retrived article titles and their respective summaries

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ var searchButton;
 var searchQuery;
 var results;
 
+// Strings to compare old and new queries.
+var query;
+
 /*
  * Initialize objects and click actions
  */
@@ -26,10 +29,12 @@ $(document).ready(function () {
  */
 function searchButtonOnClick() {
     // console.log("click");
+    query = searchQuery.val();
 
-    var query = searchQuery.val();
+    if (!query) {
+        return;
+    }
     // console.log(query);
-
     $.ajax({
         url:'http://en.wikipedia.org/w/api.php',
         data: {
@@ -40,26 +45,42 @@ function searchButtonOnClick() {
             srsearch: query
         },
         dataType: 'jsonp',
-        success: processResult
+        success: function(data) {
+            // Array of search results
+            const search = data["query"]["search"];
+            //console.log(search);
+
+            // String used in titles field of 'data' in upcoming ajax call.
+            // Form: "title1|title2|title3|...|lasttitle"
+            const titles = search.map(searchItem => searchItem["title"]).join('|');
+            //console.log(titles);
+            $.ajax({
+                url:'http://en.wikipedia.org/w/api.php',
+                data: {
+                    action: 'query',
+                    format: 'json',
+                    prop: 'extracts',
+                    titles: titles,
+                    exsentences: '1',
+                    exlimit: '20',
+                    exintro: '1',
+                    explaintext: '1'
+                },
+                dataType: 'jsonp',
+                success: function(data2) {
+                    const pages = data2["query"]["pages"];
+                    //console.log(pages);
+                    const markup = `
+                    ${search.map(searchItem =>
+                        `<li>
+                            <a href="https://en.wikipedia.org/wiki/${searchItem["title"].replace(' ', '_')}">${searchItem["title"]}</a>
+                            <h5>${pages[searchItem["pageid"] + ""].extract}</h5>
+                         </li>`)
+                        .join('')}`;
+                    //console.log(markup);
+                    results.html(markup);
+                }
+            });
+        }
     });
-}
-
-/*
- * Update HTML with relevant data
- */
-function processResult(data) {
-    // console.log(data["query"]["search"]);
-    var pages = data["query"]["search"];
-    var titles = [];
-    var ids = [];
-
-    results.text("");
-
-    for (var i = 0; i<pages.length; i++) {
-        titles.push(pages[i]["title"]);
-        ids.push(pages[i]["pageid"]);
-
-        results.append("<li>" + titles[i] + "</li>");
-    }
-    // console.log(titles);
 }
